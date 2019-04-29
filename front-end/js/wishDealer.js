c86a58b76b44cd68a80058e06db8cab0fac4e7f2
Chenyu Xi <xi_chenyu@163.com>
2019-04-29 09:24:36 -0400

    commit an json file to api gateway

--- front-end/js/wishDealer.js
+++ front-end/js/wishDealer.js
@@ -1,19 +1,22 @@
-function addNewTrip(callback) {
+test_people_id = "test";
+
+function addNewTrip() {
     let place = $('#place').val();
     let idea = $('#place_idea').val();
     let date = new Date($('#date').val());
     let currentDate = new Date();
-    if(currentDate.getTime() > date.getTime()){
+    if (currentDate.getTime() > date.getTime()) {
         alert("The plan date is before today!");
         $("#closeAdd").trigger("click");
     }
-    else if (date.toLocaleDateString()==="Invalid Date" || place===""){
+    else if (date.toLocaleDateString() === "Invalid Date" || place === "") {
         alert("Miss required field");
         $("#closeAdd").trigger("click");
 
     }
     else {
         let doc = {
+            "peopleID": test_people_id,
             "place": place,
             "idea": idea,
             "timeStamp": currentDate.toLocaleDateString(),
@@ -21,13 +24,8 @@ function addNewTrip(callback) {
         };
         console.log(doc);
         alert("add!");
+        $("#closeAdd").trigger("click");
 
         // Todo: add json to DynamoDB
     }
-
-    callback();
-}
-
-function addToDB() {
-    $("#closeAdd").trigger("click");
 }
