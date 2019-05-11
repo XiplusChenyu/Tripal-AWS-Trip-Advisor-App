327e081ece2570899603d08bbfd85fba41a5b52b
Jiliang Ma <majiliang555@gmail.com>
2019-05-11 15:29:13 -0400

    finish flight_lambda

--- back-end/flight/flight_jump_page.py
+++ back-end/flight/flight_jump_page.py
@@ -1,27 +1,25 @@
 import json
-import random
 
 
-def flightsearch(date, departure = 'NYC' ,arrival ='BOS' ):
-    aplis = []
-    with open('/Users/jiliangma/Desktop/proj/airports.json', 'r') as f:
+def flightsearch(date, departure ,arrival):
+
+    departure_code = departure
+    arrival_code = arrival
+    with open('airports.json', 'r') as f:
         d = json.load(f)
         for i in range(len(d)):
-            aplis.append(d[i]['iata'])
+            if departure.lower() == d.values()[i]["city"].lower() and "International".lower() in d.values()[i]["name"].lower():
+                departure_code = d.values()[i]["iata"]
+            if arrival.lower() == d.values()[i]["city"].lower() and "International".lower() in d.values()[i]["name"].lower():
+                arrival_code = d.values()[i]["iata"]
 
-
-    # print(json.dumps(aplis, indent = 2))
     link = 'https://www.kayak.com/flights/'
 
-    link += departure
-    # link += aplis[random.randint(0,len(aplis))]
-
+    link += departure_code
 
     link += '-'
 
-    link += arrival
-    # link += aplis[random.randint(0,len(aplis))]
-
+    link += arrival_code
 
     link += '/'
 
@@ -33,6 +31,4 @@ def flightsearch(date, departure = 'NYC' ,arrival ='BOS' ):
 
     return link
 
-print(flightsearch( date =  '2019-06-12'))
-
-# https://www.kayak.com/flights/NYC-JFK/2019-05-10?sort=bestflight_a
+print(flightsearch(departure="san diego", arrival="Seattle", date =  '2019-06-12'))
--- back-end/flight/flight_lambda.py
+++ back-end/flight/flight_lambda.py
@@ -7,7 +7,49 @@ import boto3
 
 def lambda_handler(event, context):
 
-    response = unirest.get("https://skyscanner-skyscanner-flight-search-v1.p.rapidapi.com/apiservices/browseroutes/v1.0/US/USD/en-US/SFO/JFK/2019-05-20/2019-09-12",
+    departure = event["depart"]
+    arrival = event["arrival"]
+    departure_date = event["depart_date"]
+    back_date = event["back_date"]
+    
+    
+    
+    with open('airports.json', 'r') as f:
+        d = json.load(f)
+        for i in range(len(d)):
+            if departure.lower() == d.values()[i]["city"].lower() and "International".lower() in d.values()[i]["name"].lower():
+                departure_code = d.values()[i]["iata"]
+            if arrival.lower() == d.values()[i]["city"].lower() and "International".lower() in d.values()[i]["name"].lower():
+                arrival_code = d.values()[i]["iata"]
+    
+    link = 'https://www.kayak.com/flights/'
+
+    link += departure_code
+
+    link += '-'
+
+    link += arrival_code
+
+    link += '/'
+
+    link += departure_date
+
+    link += '/'
+
+    link += 'sort=bestflight_a'
+
+    # return link
+
+
+
+    api_link = "https://skyscanner-skyscanner-flight-search-v1.p.rapidapi.com/apiservices/browseroutes/v1.0/US/USD/en-US/"
+
+    api_link += departure_code + "/" + arrival_code + "/"+ departure_date + "/" + back_date
+
+    print(api_link)
+
+    # # response = unirest.get("https://skyscanner-skyscanner-flight-search-v1.p.rapidapi.com/apiservices/browseroutes/v1.0/US/USD/en-US/SFO/JFK/2019-05-20/2019-09-12",
+    response = unirest.get(api_link,
       headers={
         "X-RapidAPI-Host": "skyscanner-skyscanner-flight-search-v1.p.rapidapi.com",
         "X-RapidAPI-Key": "4db5077f80msh474ba85a6bff987p1816a6jsna75506094f1d"
@@ -15,10 +57,10 @@ def lambda_handler(event, context):
     )
     
     temp = response.body
-    
-    currency_symbol = ''
-    departure = ''
-    arrival = ''
+    # print(temp)
+    # currency_symbol = ''
+    # departure = ''
+    # arrival = ''
     
     route = []
     
@@ -26,14 +68,11 @@ def lambda_handler(event, context):
     for i in range(len(temp["Carriers"])):
         carrier_dic.update({temp["Carriers"][i]["CarrierId"]:temp["Carriers"][i]["Name"]})
     
-    currency = temp["Currencies"][0]["Code"]
-    departure = temp["Places"][0]["CityName"]
-    arrival = temp["Places"][1]["CityName"]
     
     for i in range(len(temp["Quotes"])):
         route += [{"price":temp["Quotes"][i]["MinPrice"],"carrier":carrier_dic[temp["Quotes"][i]["OutboundLeg"]["CarrierIds"][0]],"departure_date":temp["Quotes"][i]["OutboundLeg"]["DepartureDate"][:10],"back_date":temp["Quotes"][i]["InboundLeg"]["DepartureDate"][:10]}]
-    # print(route)
-    # route = json.loads(route)
-    # print(json.dumps(route,indent = 2))
-    return route
+
     
+    route += [{"kayak_link":link}]
+    
+    return route
